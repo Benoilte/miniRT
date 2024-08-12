8b0ec0f9fae6eadbf8e8fb88e47dce3af6cd7ee5
Ben Golding <bgolding.42lausanne@gmail.com>
2024-08-12 14:24:31 +0200

    New encapsulating header: librt.h

---
+++ lib/librt/inc/librt.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   librt.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 14:22:46 by bgolding          #+#    #+#             */
+/*   Updated: 2024/08/12 14:23:22 by bgolding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBRT_H
+# define LIBRT_H
+
+# include "tuples.h"
+# include "matrix.h"
+# include "colors.h"
+# include "math_utils.h"
+
+#endif
