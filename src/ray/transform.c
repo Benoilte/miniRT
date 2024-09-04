a903c2854b881848fede5a48c5e429df392d6f5c
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-04 12:07:23 +0200

    New function(file): transform_ray() (ray/transform.c)
    Returns a ray that is the result of applying a matrix transformation to both elements (origin & direction) of
    a ray passed as an argument.
    
    Prototype added to ray.h

--- src/ray/transform.c
+++ src/ray/transform.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ray.h                                              :+:      :+:    :+:   */
+/*   transform.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/12 16:00:33 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/04 12:06:55 by bgolding         ###   ########.fr       */
+/*   Created: 2024/09/04 12:03:00 by bgolding          #+#    #+#             */
+/*   Updated: 2024/09/04 12:06:58 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RAY_H
-# define RAY_H
+#include "minirt.h"
 
-//	INCLUDES
-
-# include "minirt.h"
-
-//	TYPEDEFS
-
-typedef struct s_ray
+t_ray	transform_ray(t_ray *r, t_m4x4 m)
 {
-	t_point		origin;
-	t_vector	direction;
-}	t_ray;
-
-//	PROTOTYPES
-
-t_ray	ray(t_point	origin, t_vector direction);
-t_point	ray_position(t_ray ray, float t);
-t_ray	transform_ray(t_ray *r, t_m4x4 m);
-
-#endif
+	return (ray(mx_mult_tuple(m, r->origin), mx_mult_tuple(m, r->direction)));
+}
