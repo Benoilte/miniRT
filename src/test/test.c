7995dc33bb1cd5e91775c851b0a1ce5481fc9b9d
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-06 19:17:38 +0200

    Added render timer to test main
    timed_render(t_data *data);
    
    Makes a call to render() but times the time elapsed with the use of the
    <time.h> library.
    
    This function could be called from within a key/mouse hook but would
    require declaring the prototype in a main part of the program.
    
    BE SURE TO NOT ADD THIS FEATURE TO THE COMPULSORY PART OF THE PROJECT AS
    IT CONTAINS AN UNAUTHORIZED FUNCTION/LIBRARY.

--- src/test/test.c
+++ src/test/test.c
@@ -3,18 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:49:32 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/09/05 16:12:50 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/09/06 19:16:42 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include <time.h>
 
 void	set_default_scene(t_data *data);
 void	set_first_scene(t_data *data);
 void	test_shadow(t_data *data);
+
+void	timed_render(t_data *data)
+{
+	clock_t	start;
+	double	end;
+
+	start = clock();
+	render(data);
+	end = (double)(clock() - start) / CLOCKS_PER_SEC;
+	printf("Render time: %.2f seconds\n", end);
+}
 
 int	main(int argc, char **argv)
 {
@@ -33,7 +45,7 @@ int	main(int argc, char **argv)
 		test_shadow(data);
 	else
 		return (0);
-	render(data);
+	timed_render(data);
 	mlx_loop(data->mlx->xvar);
 	destroy_data(data);
 	return (0);
