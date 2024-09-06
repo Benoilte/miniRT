2adcc3d6bfa9629e98037464b0eb3be71ea616bd
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-06 19:21:18 +0200

    Optimization of mx_mult_tuple & dot_product_row_tuple
    mx_mult_tuple:
    
    Now declares a local t_tuple to store the values obtained through
    dot_product_row_tuple rather than typecasting a litteral inside the
    return statement.
    
    Operation count and runtime (of function) reduced by a non-negligeable amount as a result (approx
    20-30%).
    
    dot_product_row_tuple:
    
    Now takes a pointer to a t_tuple rather than the struct.
    
    Operation count and runtime (of function) reduced marginally as a result
    (every little helps). As a static function there is little risks of
    adverse effects by applying small optimizations.

--- lib/libgraphic/src/matrix/multiply.c
+++ lib/libgraphic/src/matrix/multiply.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:55:50 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/08/12 11:03:06 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/09/06 17:42:32 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,18 @@ t_m4x4	mx_mult(t_m4x4 a, t_m4x4 b)
 	return (new);
 }
 
-static float	dot_product_row_tuple(float *row, t_tuple t)
+static float    dot_product_row_tuple(float *row, t_tuple *t)
 {
-	return ((row[0] * t.x) + (row[1] * t.y) + (row[2] * t.z) + (row[3] * t.w));
+    return ((row[0] * t->x) + (row[1] * t->y) + (row[2] * t->z) + (row[3] * t->w));
 }
 
 t_tuple	mx_mult_tuple(t_m4x4 a, t_tuple tuple)
 {
-	return ((t_tuple){\
-		dot_product_row_tuple(a.data[0], tuple),
-		dot_product_row_tuple(a.data[1], tuple),
-		dot_product_row_tuple(a.data[2], tuple),
-		dot_product_row_tuple(a.data[3], tuple)});
+	t_tuple t;
+
+    t.x = dot_product_row_tuple(a.data[0], &tuple);
+    t.y = dot_product_row_tuple(a.data[1], &tuple);
+    t.z = dot_product_row_tuple(a.data[2], &tuple);
+    t.w = dot_product_row_tuple(a.data[3], &tuple);
+	return (t);
 }
