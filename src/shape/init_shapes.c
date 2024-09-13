8ad10fa7e784f6e8fe6a3408409471b867c9e373
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-13 17:34:35 +0200

    Fix to init_shapes
    Removed comments
    
    Added check: if no shapes in list, returns an error (this should be
    handled upstream).

--- src/shape/init_shapes.c
+++ src/shape/init_shapes.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 22:17:34 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/13 17:25:41 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/09/13 17:34:21 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,18 @@ static t_shape_type	get_type(t_id id)
 
 int	init_shapes(t_world *world, t_list *token_list)
 {
-	/*
-		Iterate over tokens
-			if shape
-				add_new_shape_to_world[type]
-				set_shape[type] << const function array
-	*/
 	t_list	*shape_token;
 	t_shape	*new_shape;
 
 	shape_token = get_next_shape(token_list);
+	if (!shape_token)
+		return (print_error("init_shapes", "no shapes in list"));
 	while (shape_token)
 	{
 		(void)new_shape;
 		(void)world;
-		ft_printf("Placeholder for add_new_shape_to_world(%d)\n", get_type(((t_token *)shape_token->content)->identifier));
+		ft_printf("Placeholder for add_new_shape_to_world(%d)\n", \
+			get_type(((t_token *)shape_token->content)->identifier));
 		shape_token = get_next_shape(shape_token->next);
 	}
 	return (0);
