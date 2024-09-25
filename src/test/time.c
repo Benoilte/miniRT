36cc4dfe78554ddcdc5ec2fd58c4e465f77b99e9
benoit brandt <benoit.07@hotmail.com>
2024-09-25 11:58:14 +0200

    Keep usefull function into test folder

---
+++ src/test/time.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 11:53:13 by bebrandt          #+#    #+#             */
+/*   Updated: 2024/09/25 11:53:45 by bebrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+#include <time.h>
+
+void	timed_render(t_data *data)
+{
+	clock_t	start;
+	double	end;
+
+	start = clock();
+	render(data);
+	end = (double)(clock() - start) / CLOCKS_PER_SEC;
+	printf("Render time: %.2f seconds\n", end);
+}
+
