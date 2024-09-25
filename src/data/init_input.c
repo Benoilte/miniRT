f8fb8ab4e97d2b90444510a0e5afeb670b5d4bf6
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-25 11:29:31 +0200

    Removed token print display (parsing)
    Moved test functions print_token() & print_args() to src/test/print.c
    
    Removed interative function call at the end of init_input() that was
    triggering the display.

--- src/data/init_input.c
+++ src/data/init_input.c
@@ -6,40 +6,11 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 14:12:51 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/19 14:44:52 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/09/25 11:29:07 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-// FOR TESTING PURPOSES
-void	print_args(char **args)
-{
-	int	i;
-
-	if (!args)
-		ft_printf("No args");
-	i = 0;
-	while (args[i])
-		ft_printf("[%s] ", args[i++]);
-	ft_putchar_fd('\n', STDOUT_FILENO);
-}
-
-// FOR TESTING PURPOSES
-void	print_token(void *content)
-{
-	t_token	*token;
-
-	if (!content)
-		return ;
-	token = (t_token *)content;
-	ft_printf("=== <TOKEN> ===\n");
-	ft_printf("identifier:\t%d\n", token->identifier);
-	ft_printf("args:\t\t");
-	print_args(token->args);
-	ft_printf("line:\t\t%d\n", token->line);
-	ft_printf("=== <END> ===\n\n");
-}
 
 int	init_input(t_input_data *input, int argc, char **argv)
 {
@@ -53,6 +24,5 @@ int	init_input(t_input_data *input, int argc, char **argv)
 		return (4);
 	if (parser(input) != 0)
 		return (5);
-	ft_lstiter(input->token_list, print_token);
 	return (0);
 }
--- src/test/print.c
+++ src/test/print.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:06:53 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/09/10 16:21:53 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/09/25 11:28:52 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,31 @@ void	print_tuple(t_tuple a, char *msg)
 {
 	printf("\t%-20s: x: %- 10.5f y: %- 10.5f ", msg, a.x, a.y);
 	printf("z: %- 10.5f w: %- 10.5f\n", a.z, a.w);
+}
+
+void	print_args(char **args)
+{
+	int	i;
+
+	if (!args)
+		ft_printf("No args");
+	i = 0;
+	while (args[i])
+		ft_printf("[%s] ", args[i++]);
+	ft_putchar_fd('\n', STDOUT_FILENO);
+}
+
+void	print_token(void *content)
+{
+	t_token	*token;
+
+	if (!content)
+		return ;
+	token = (t_token *)content;
+	ft_printf("=== <TOKEN> ===\n");
+	ft_printf("identifier:\t%d\n", token->identifier);
+	ft_printf("args:\t\t");
+	print_args(token->args);
+	ft_printf("line:\t\t%d\n", token->line);
+	ft_printf("=== <END> ===\n\n");
 }
