572a1b32212b4aaacfc3cb2932da23780df0eb50
Ben Golding <bgolding.42lausanne@gmail.com>
2024-09-30 16:28:13 +0200

    removed redundant mutex destroy in destroy_data.c

--- bonus/src/data/destroy_data.c
+++ bonus/src/data/destroy_data.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:05:55 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/30 13:51:58 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:26:08 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@ void	destroy_data(t_data *data)
 	destroy_mlx(data->mlx);
 	destroy_world(data->world);
 	destroy_camera(data->camera);
-	pthread_mutex_destroy(&data->set_pixel_mutex);
 	free(data);
 }
