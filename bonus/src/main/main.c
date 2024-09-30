93d88d59b545648e3e5e9e8bad5f6cec66eea900
Benoilte <benoit.07@hotmail.com>
2024-09-30 16:57:26 +0200

    add test for reflection

--- bonus/src/main/main.c
+++ bonus/src/main/main.c
@@ -3,30 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 11:20:16 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/30 11:14:21 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:28:31 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void	test_reflective_value(t_world *world);
-void	test_reflective_vector(t_world *world);
-void	test_non_rflective_material(t_world *world);
+void	test_reflective_value(t_world *world, float value);
+
+static void	update_shape(t_world *world)
+{
+	t_shape *shape1;
+	t_shape *shape2;
+
+	shape1 = ((t_shape *)(world->shapes->content));
+	shape2 = ((t_shape *)(world->shapes->next->content));
+	shape1->material.reflective = 0.2;
+	shape2->material.reflective = 0.2;
+}
 
 int	main(int argc, char **argv)
 {
 	t_data				*data;
 
 	data = init_data(argc, argv);
-	test_reflective_value(data->world);
-	test_reflective_vector(data->world);
-	test_non_rflective_material(data->world);
-	// set_hooks(data);
-	// render(data);
-	// mlx_loop(data->mlx->xvar);
+	update_shape(data->world);
+	test_reflective_value(data->world, 0.5);
+	set_hooks(data);
+	render(data);
+	mlx_loop(data->mlx->xvar);
 	destroy_data(data);
 	return (0);
 }
--- bonus/src/test/reflection_and_refraction.c
+++ bonus/src/test/reflection_and_refraction.c
@@ -3,19 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   reflection_and_refraction.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:28:44 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/09/30 11:36:28 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:26:07 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void	test_reflective_value(t_world *world)
+void	print_color(t_color c, char *msg);
+
+void	test_reflective_value(t_world *world, float value)
 {
-	printf("check if reflective value is equal to 0,0\n");
-	if (equalf(((t_shape *)(world->shapes->content))->material.reflective, 0.0))
+	t_shape	*shape;
+	
+	shape = ((t_shape *)(world->shapes->next->content));
+	printf("check if reflective value is equal to %f\n", value);
+	if (equalf(shape->material.reflective, value))
 		printf("\033[0;32mTRUE\033[0m\n");
 	else
 		printf("\033[0;31mFALSE\033[0m\n");
@@ -46,7 +51,7 @@ void	test_reflective_vector(t_world *world)
 	destroy_shape(shape);
 }
 
-void	test_non_rflective_material(t_world *world)
+void	test_non_reflective_material(t_world *world)
 {
 	t_ray			r1;
 	t_shape			*shape;
@@ -59,7 +64,8 @@ void	test_non_rflective_material(t_world *world)
 	inter = set_intersection(shape, 1);
 	if (compute_details(&details, &inter, r1, world) != 0)
 		return ;
-	color = reflected_color(world, &details);
+	if (reflected_color(&color, world, &details) != 0)
+		return ;
 	printf("check if non reflective material return a black color\n");
 	if (rgb_equal(color, rgb_set(0, 0, 0)))
 		printf("\033[0;32mTRUE\033[0m\n");
@@ -67,7 +73,47 @@ void	test_non_rflective_material(t_world *world)
 		printf("\033[0;31mFALSE\033[0m\n");
 }
 
-// test_reflective_material(t_world *world)
-// {
-	
-// }
+void	test_reflective_material(t_world *world)
+{
+	t_ray			r1;
+	t_shape			*shape;
+	t_intersection	inter;
+	t_details		details;
+	t_color			color;
+
+	shape = ((t_shape *)(world->shapes->next->content));
+	shape->material.reflective = 0.5;
+	r1 = ray(point(0, 0, -3), vector(0, -(sqrtf(2)) / 2, (sqrtf(2)) / 2));
+	inter = set_intersection(shape, sqrtf(2));
+	if (compute_details(&details, &inter, r1, world) != 0)
+		return ;
+	if (color_at(&color, &r1, world) != 0)
+		return ;
+	printf("check if reflective material return a color(0, 0.67726, 0)\n");
+	if (rgb_equal(color, rgb_set(0, 0.67726, 0)))
+		printf("\033[0;32mTRUE\033[0m\n");
+	else
+	{
+		printf("\033[0;31mFALSE\033[0m\n");
+		print_color(color, "reflected_color");
+	}
+}
+
+void	test_mutually_reflective_material(t_world *world)
+{
+	t_ray			r1;
+	t_color			color;
+	t_shape			*pl_upper;
+	t_shape			*pl_lower;
+
+	pl_upper = ((t_shape *)(world->shapes->content));
+	pl_lower = ((t_shape *)(world->shapes->next->content));
+	pl_upper->material.reflective = 1;
+	pl_lower->material.reflective = 1;
+	r1 = ray(point(0, 0, 0), vector(0, 1, 0));
+	if (color_at(&color, &r1, world) != 0)
+		return ;
+	printf("check if mutually reflective material is not segfault\n");
+	if (rgb_equal(color, rgb_set(0, 0.67726, 0)))
+		printf("\033[0;32mTRUE\033[0m\n");
+}
