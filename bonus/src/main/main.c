fb76825733a782acbd31488c82c8bda5cfb2a3cb
Benoilte <benoit.07@hotmail.com>
2024-10-01 12:17:53 +0200

    add temporary test for transparency and reraction

--- bonus/src/main/main.c
+++ bonus/src/main/main.c
@@ -3,20 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:05:00 by bgolding          #+#    #+#             */
-/*   Updated: 2024/09/30 19:05:03 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:17:04 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	test_transparency_value(t_world *world, float value);
+void	test_refraction_value(t_world *world, float value);
 
 int	main(int argc, char **argv)
 {
 	t_data				*data;
 
 	data = init_data(argc, argv);
+	test_transparency_value(data->world, 0);
+	test_refraction_value(data->world, 1);
 	set_hooks(data);
 	timed_render(data);
 	mlx_loop(data->mlx->xvar);
--- bonus/src/test/reflection_and_refraction.c
+++ bonus/src/test/reflection_and_refraction.c
@@ -6,7 +6,7 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:28:44 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/09/30 18:52:33 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/01 12:15:36 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,30 @@ void	test_reflective_value(t_world *world, float value)
 	shape = ((t_shape *)(world->shapes->next->content));
 	printf("check if reflective value is equal to %f\n", value);
 	if (equalf(shape->material.reflective, value))
+		printf("\033[0;32mTRUE\033[0m\n");
+	else
+		printf("\033[0;31mFALSE\033[0m\n");
+}
+
+void	test_transparency_value(t_world *world, float value)
+{
+	t_shape	*shape;
+
+	shape = ((t_shape *)(world->shapes->content));
+	printf("check if reflective value is equal to %f\n", value);
+	if (equalf(shape->material.transparency, value))
+		printf("\033[0;32mTRUE\033[0m\n");
+	else
+		printf("\033[0;31mFALSE\033[0m\n");
+}
+
+void	test_refraction_value(t_world *world, float value)
+{
+	t_shape	*shape;
+
+	shape = ((t_shape *)(world->shapes->content));
+	printf("check if reflective value is equal to %f\n", value);
+	if (equalf(shape->material.refractive_index, value))
 		printf("\033[0;32mTRUE\033[0m\n");
 	else
 		printf("\033[0;31mFALSE\033[0m\n");
