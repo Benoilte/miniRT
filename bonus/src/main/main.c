054ef886a0238c5c90194b9b4011ecaa02aed2e1
benoit brandt <benoit.07@hotmail.com>
2024-10-04 10:55:18 +0200

    add temporary test for main bonus part

--- bonus/src/main/main.c
+++ bonus/src/main/main.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:05:00 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/01 15:37:02 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/04 00:04:25 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	test_refraction_index(t_data *data);
 
 int	main(int argc, char **argv)
 {
@@ -18,6 +20,7 @@ int	main(int argc, char **argv)
 
 	data = init_data(argc, argv);
 	set_hooks(data);
+	test_refraction_index(data);
 	timed_render(data);
 	mlx_loop(data->mlx->xvar);
 	destroy_data(data);
