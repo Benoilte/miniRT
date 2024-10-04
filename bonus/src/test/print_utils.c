73d949daeacd50d324de0797ba828c0c0477da7f
benoit brandt <benoit.07@hotmail.com>
2024-10-04 09:37:19 +0200

    update && add test for refraction

---
+++ bonus/src/test/print_utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 09:05:55 by bebrandt          #+#    #+#             */
+/*   Updated: 2024/10/04 09:06:19 by bebrandt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	print_n1_and_n2(t_dbl_lst *intersects, t_ray *ray, t_dbl_lst *containers)
+{
+	t_dbl_lst	*tmp;
+	t_details	details;
+	int			i;
+
+	i = 0;
+	tmp = intersects;
+	printf("| i |\t|   n1    |\t|   n2    |\n");
+	while (tmp)
+	{
+		compute_details(&details, ((t_intersection *)tmp->content), *ray);
+		compute_refraction_indices(intersects, &details, containers);
+		printf("| %d |\t| %.5f |\t| %.5f |\n", i, details.n1, details.n2);
+		tmp = tmp->next;
+		i++;
+	}
+}
--- bonus/src/test/reflection_and_refraction.c
+++ bonus/src/test/reflection_and_refraction.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   reflection_and_refraction.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:28:44 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/10/01 14:11:04 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/04 01:16:47 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
 void	print_color(t_color c, char *msg);
+void	print_n1_and_n2(t_dbl_lst *intersects, t_ray *ray, t_dbl_lst *containers);
 
 void	test_reflective_value(t_world *world, float value)
 {
@@ -50,24 +51,24 @@ void	test_refraction_value(t_world *world, float value)
 		printf("\033[0;31mFALSE\033[0m\n");
 }
 
-void	test_refraction_index(t_world *world)
+void	test_refraction_index(t_data *data)
 {
 	t_ray				r1;
 	t_intersect_list	*intersects;
 	t_intersect_list	*tmp;
 	t_details			details;
-	
-	r1 = ray(point(0, 0, -4), vector(0, 0, 1));
 
+	r1 = ray(point(0, 0, -4), vector(0, 0, 1));
 	ft_bzero(&details, sizeof(details));
 	intersects = NULL;
-	if (intersect_world(&intersects, &r1, world) != 0)
+	if (intersect_world(&intersects, &r1, data->world) != 0)
 		return ;
 	tmp = intersects;
 	while (tmp)
 	{
-		printf("%p: %.5f\n", ((t_intersection *)tmp->content)->shape, ((t_intersection *)tmp->content)->t);
+		printf("%p: %.5f - %.5f\n", ((t_intersection *)tmp->content)->shape, ((t_intersection *)tmp->content)->t, ((t_intersection *)tmp->content)->shape->material.refractive_index);
 		tmp = tmp->next;
 	}
+	print_n1_and_n2(intersects, &r1, data->render[0].shape_container);
 	dbl_lstclear(&intersects, clear_intersection);
 }
