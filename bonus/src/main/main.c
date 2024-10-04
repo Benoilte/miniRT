b9a306a341cc6bf0badde7e7a61ec9180652c3aa
Benoilte <benoit.07@hotmail.com>
2024-10-04 16:22:17 +0200

    restore default main

--- bonus/src/main/main.c
+++ bonus/src/main/main.c
@@ -6,14 +6,11 @@
 /*   By: bebrandt <bebrandt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:05:00 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/04 11:22:27 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/04 16:21:47 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-void	test_refraction_index(t_data *data);
-void	test_details_under_point(t_data *data);
 
 int	main(int argc, char **argv)
 {
@@ -21,8 +18,6 @@ int	main(int argc, char **argv)
 
 	data = init_data(argc, argv);
 	set_hooks(data);
-	test_refraction_index(data);
-	test_details_under_point(data);
 	timed_render(data);
 	mlx_loop(data->mlx->xvar);
 	destroy_data(data);
