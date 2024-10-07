75aeb9a50952bae7565bb58a500243d59273d213
Ben Golding <bgolding.42lausanne@gmail.com>
2024-10-07 13:11:31 +0200

    Hotfix: removed redundancies in render()
    Removed reset of reflective_depth & refractive_depth at each iteration.
    
    This is no longer a problem since the info is passed by value to
    compute_final_color() inside color_at().

--- bonus/src/rendering/render.c
+++ bonus/src/rendering/render.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 12:57:38 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/07 12:14:05 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/07 13:06:47 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,6 @@ void	*render_strip(void *arg)
 		while (++(pixel.x) < WIN_WIDTH)
 		{
 			ray = ray_for_pixel(*(info->data->camera), pixel.x, pixel.y);
-			info->reflective_depth = REFLECTIVE_DEPTH;
-			info->refractive_depth = REFRACTIVE_DEPTH;
 			if (color_at(&color, NULL, &ray, info) != 0)
 				return (print_error("render_strip", "aborting thread"), NULL);
 			color_int = rgb_stoi(color);
