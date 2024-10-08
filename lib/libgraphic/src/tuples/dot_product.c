2b1dc7175e3057e57b1777d89fe7d98464e4be77
Ben Golding <bgolding.42lausanne@gmail.com>
2024-10-08 16:33:47 +0200

    libgraphic: tp_dot_product optimization
    Removed check for correct usage that was causing a major slow-down
    upstream.
    
    It is now the caller's responsibility to ensure the arguments passed are
    t_vector (w == VECTOR).
    
    This removal results in a 10-40% gain in rendering time.

--- lib/libgraphic/src/tuples/dot_product.c
+++ lib/libgraphic/src/tuples/dot_product.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 09:42:51 by bgolding          #+#    #+#             */
-/*   Updated: 2024/08/09 11:14:44 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/08 16:33:36 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 float	tp_dot_product(t_vector a, t_vector b)
 {
-	if (!equalf(a.w, VECTOR) || !equalf(b.w, VECTOR))
-		tp_error("tp_dot_product", TP_DOT_ERROR);
 	return ((a.x * b.x) + (a.y * b.y) + (a.z * b.z));
 }
