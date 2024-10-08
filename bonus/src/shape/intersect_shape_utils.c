04f8595e8ebef4f43e30e2af1f10a1dd956c24b0
benoit brandt <benoit.07@hotmail.com>
2024-10-08 16:07:44 +0200

    Hotfix - use fmaxf function to compare the bigger float

--- bonus/src/shape/intersect_shape_utils.c
+++ bonus/src/shape/intersect_shape_utils.c
@@ -6,9 +6,11 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:58:24 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/10/08 13:30:05 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/08 15:58:48 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minirt.h"
 
 void	ft_swap(float *a, float *b)
 {
@@ -21,9 +23,9 @@ void	ft_swap(float *a, float *b)
 
 float	ft_get_max_f(float x, float y, float z)
 {
-	if ((x > y) && (x > z))
+	if ((equalf(fmaxf(x, y), x)) && (equalf(fmaxf(x, z), x)))
 		return (x);
-	if ((y > x) && (y > z))
+	if ((equalf(fmaxf(y, x), y)) && (equalf(fmaxf(y, z), y)))
 		return (y);
 	else
 		return (z);
@@ -31,9 +33,9 @@ float	ft_get_max_f(float x, float y, float z)
 
 float	ft_get_min_f(float x, float y, float z)
 {
-	if ((x < y) && (x < z))
+	if ((equalf(fmaxf(x, y), y)) && (equalf(fmaxf(x, z), z)))
 		return (x);
-	if ((y < x) && (y < z))
+	if ((equalf(fmaxf(y, x), x)) && (equalf(fmaxf(y, z), z)))
 		return (y);
 	else
 		return (z);
