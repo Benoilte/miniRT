7bd0aeb06dcad332015e78e55dfd05338f53ecc5
benoit brandt <benoit.07@hotmail.com>
2024-10-08 14:46:44 +0200

    restore main as default

--- bonus/src/main/main.c
+++ bonus/src/main/main.c
@@ -6,23 +6,20 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 19:05:00 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/08 09:39:40 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/10/08 14:45:43 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-void	test_intersect_cube(t_data *data);
 
 int	main(int argc, char **argv)
 {
 	t_data				*data;
 
 	data = init_data(argc, argv);
-	// set_hooks(data);
-	// timed_render(data);
-	// mlx_loop(data->mlx->xvar);
-	test_intersect_cube(data);
+	set_hooks(data);
+	timed_render(data);
+	mlx_loop(data->mlx->xvar);
 	destroy_data(data);
 	return (0);
 }
