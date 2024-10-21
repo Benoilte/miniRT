b742db8590a5f9c95eb1ce570176835697fd9454
Ben Golding <bgolding.42lausanne@gmail.com>
2024-10-21 15:32:31 +0200

    destroy_data now restores key_autorepeat

--- bonus/src/data/destroy_data.c
+++ bonus/src/data/destroy_data.c
@@ -6,7 +6,7 @@
 /*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:05:55 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/18 15:05:16 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/21 15:31:51 by bgolding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	destroy_data(t_data *data)
 {
 	if (!data)
 		return ;
+	mlx_do_key_autorepeaton(data->mlx->xvar);
 	destroy_input(&data->input);
 	destroy_mlx(data->mlx);
 	destroy_world(data->world);
