620353c9b101960da5903af7d548d8b24727dca1
benoit brandt <benoit.07@hotmail.com>
2024-10-22 12:23:46 +0200

    Hotfix - Segfault when launching ./minirt_bonus without argument
    The segfault was the result of calling function mlx_do_key_autorepeaton(mlx->xvar);
    with mlx->xvar NULL
    
    Now the function is called only if mlx->xvar is not NULL

--- bonus/src/data/destroy_data.c
+++ bonus/src/data/destroy_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   destroy_data.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 12:05:55 by bgolding          #+#    #+#             */
-/*   Updated: 2024/10/21 15:31:51 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:23:06 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	destroy_data(t_data *data)
 {
 	if (!data)
 		return ;
-	mlx_do_key_autorepeaton(data->mlx->xvar);
 	destroy_input(&data->input);
 	destroy_mlx(data->mlx);
 	destroy_world(data->world);
--- bonus/src/window/destroy_mlx_macos.c
+++ bonus/src/window/destroy_mlx_macos.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   destroy_mlx_macos.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgolding <bgolding@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 10:12:56 by bgolding          #+#    #+#             */
-/*   Updated: 2024/08/23 16:29:11 by bgolding         ###   ########.fr       */
+/*   Updated: 2024/10/22 11:05:19 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,11 @@ void	destroy_mlx(t_mlx *mlx)
 			mlx_destroy_window(mlx->xvar, mlx->win);
 			mlx->win = NULL;
 		}
-		mlx->xvar = NULL;
+		if (mlx->xvar)
+		{
+			mlx_do_key_autorepeaton(mlx->xvar);	
+			mlx->xvar = NULL;
+		}
 		free(mlx);
 	}
 }
